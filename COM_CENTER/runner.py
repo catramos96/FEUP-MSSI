3c94e0395f50dd863a3ad1f80020d43f4d30e46c
Pedro Dias <pmds3000@hotmail.com>
2018-06-18 22:20:55 +0100

    moving with rotation and arrows on sumo

--- COM_CENTER/runner.py
+++ COM_CENTER/runner.py
@@ -36,6 +36,15 @@ traci.vehicle.subscribe("newVeh", (tc.VAR_ROAD_ID, tc.VAR_LANEPOSITION))
 traci.vehicle.setColor("newVeh", (100,254,100,254))
 
 
+def moveCar(carID, x, y, angle):
+    keepRoute = 2
+    edgeID = ''
+    keepRoute = 1
+    lane = -1
+    pos = traci.vehicle.getPosition("newVeh")
+    old_angle = traci.vehicle.getAngle("newVeh")
+    traci.vehicle.moveToXY("newVeh", edgeID , lane , pos[0] + x, pos[1]  + y, old_angle + angle, keepRoute)
+    return;
 
 
 
@@ -48,14 +57,13 @@ for step in range(1000000):
    if char == ord('q'):
        break
    elif char == curses.KEY_RIGHT:
-     # print doesn't work with curses, use addstr instead
-
-     edgeID = traci.vehicle.getRoadID("newVeh")
-     pos = traci.vehicle.getPosition("newVeh")
-     laneIndex = traci.vehicle.getLaneIndex("newVeh")
-
-     traci.vehicle.moveToXY("newVeh", '' , -1 , pos[0], pos[1] + 0.6 , 40 , 2)
-     screen.addstr(0, 0, 'right')
+       moveCar('newVeh',1,0, 40)
+   elif char == curses.KEY_DOWN:
+       moveCar('newVeh',0,-1, 40)
+   elif char == curses.KEY_LEFT:
+       moveCar('newVeh',-1, 0, 40)
+   elif char == curses.KEY_UP:
+       moveCar('newVeh',0,1, 40)
 
    print(traci.vehicle.getSubscriptionResults("newVeh"))
    print(traci.vehicle.getPosition("newVeh"))
