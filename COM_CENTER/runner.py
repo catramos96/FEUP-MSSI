335fd34b8f6ac874de5ca0528201ca2601f64727
Pedro Dias <pmds3000@hotmail.com>
2018-06-19 22:54:31 +0100

    functions added

--- COM_CENTER/runner.py
+++ COM_CENTER/runner.py
@@ -14,6 +14,8 @@ import traci
 import traci.constants as tc
 import curses
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 screen = curses.initscr()
 
@@ -29,30 +31,81 @@ screen.keypad(True)
 
 traci.start(["sumo-gui", "-c", "../SUMO/data/hello.sumocfg"])
 traci.route.add("trip", ["E12", "E23"])
-traci.vehicle.add("newVeh","trip",typeID="reroutingType")
 
-traci.vehicle.subscribe("newVeh", (tc.VAR_ROAD_ID, tc.VAR_LANEPOSITION))
-
-traci.vehicle.setColor("newVeh", (100,254,100,254))
-
-
-def moveCar(carID, x, y, angle):
-    keepRoute = 2
-    edgeID = ''
-    keepRoute = 1
-    lane = -1
-    pos = traci.vehicle.getPosition("newVeh")
-    old_angle = traci.vehicle.getAngle("newVeh")
-    traci.vehicle.moveToXY("newVeh", edgeID , lane , pos[0] + x, pos[1]  + y, old_angle + angle, keepRoute)
+def trackCarsInJunction():
+    distance = 10
+    junctions_list = traci.junction.getIDList()
+    for junction in junctions_list:
+        traci.junction.subscribeContext(junction, tc.CMD_GET_VEHICLE_VARIABLE, distance, [tc.VAR_LANE_ID])
     return;
 
 
+def addCar(car_id, route, car_type):
+    traci.vehicle.addFull(car_id,route, car_type)
+    traci.vehicle.subscribe(car_id, (tc.VAR_ROAD_ID, tc.VAR_LANE_ID, tc.VAR_POSITION, tc.VAR_ANGLE))
+    traci.vehicle.setColor(car_id, (100,254,100,254))
+    return;
 
-print(traci.vehicle.getSubscriptionResults("newVeh"))
+
+def stopAtTrafficLights(car_id, car_lane):
+
+    lane_id = traci.vehicle.getLaneID(car_id)
+
+    junction_list = traci.junction.getIDList()
+
+    #get the cars near junction
+    cars_in_junction = {}
+    for id in junction_list:
+        junction_info = traci.junction.getContextSubscriptionResults(id)
+        if( junction_info is not None):
+            cars_in_junction.update(junction_info)
+
+    traffic_lights = traci.trafficlight.getIDList();
+
+    for light_id in traffic_lights:
+        controlled_lanes = traci.trafficlight.getControlledLanes(light_id)
+        #car in a lane controlled by trafficlight &  currente distance <= "distance"
+        pp.pprint(controlled_lanes)
+        if car_lane in controlled_lanes and car_id in cars_in_junction:
+            pp.pprint("yup, ele ta aqui")
+            pp.pprint(traci.trafficlight.getProgram(light_id))
+            pp.pprint(traci.trafficlight.getRedYellowGreenState(light_id))
+            pp.pprint(traci.trafficlight.Phase)
+
+
+
+
+
+
+
+    return False;
+
+def moveCar(car_id, x, y, angle):
+    keep_route = 2
+    edge_id = ''
+    step_info =  traci.vehicle.getSubscriptionResults(car_id)
+    lane = step_info[tc.VAR_LANE_ID]
+    pos = step_info[tc.VAR_POSITION]
+    old_angle = step_info[tc.VAR_ANGLE]
+
+    if(not stopAtTrafficLights(car_id, pos)):
+        traci.vehicle.moveToXY(car_id, edge_id , lane , pos[0] + x, pos[1]  + y, old_angle + angle, keep_route)
+    return;
+
+
+trackCarsInJunction()
+addCar("newVeh", "trip", "reroutingType")
+
 for step in range(1000000):
-   print("step", step)
+   print("step", step, "\n")
    traci.simulationStep()
-   char = screen.getch()
+
+   subs = traci.vehicle.getSubscriptionResults("newVeh")
+   pos = traci.vehicle.getPosition("newVeh")
+
+   stopAtTrafficLights("newVeh", subs[tc.VAR_LANE_ID])
+
+   char = 't';
 
    if char == ord('q'):
        break
@@ -64,9 +117,6 @@ for step in range(1000000):
        moveCar('newVeh',-1, 0, 40)
    elif char == curses.KEY_UP:
        moveCar('newVeh',0,1, 40)
-
-   print(traci.vehicle.getSubscriptionResults("newVeh"))
-   print(traci.vehicle.getPosition("newVeh"))
 
 traci.close()
 screen.close()
