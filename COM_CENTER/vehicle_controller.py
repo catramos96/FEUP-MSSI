d7269566f76eadb58b0204373829ca8f487dae7f
williamnf <wnfukunaga@gmail.com>
2018-06-22 03:05:20 +0100

    little fix

--- COM_CENTER/vehicle_controller.py
+++ COM_CENTER/vehicle_controller.py
@@ -88,5 +88,5 @@ class VehicleController:
     def send_message(self, msg):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((self.ip, self.port))
-        s.send(msg.encode("utf-8")
+        s.send(msg.encode("utf-8"))
         s.close()
